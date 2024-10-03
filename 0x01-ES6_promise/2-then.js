ffda3767b4b0fcfeae380f3e904f5764ac28cfa3
konyhea <ndukaprincek@gmail.com>
2024-10-03 02:40:02 +0100

    using then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 // handleResponseFromAPI.js
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => { // Use parentheses around the arrow function parameters
-    if (promise) {
-      return resolve({ status: 200, body: 'success' }); // Make sure 'success' is defined or use a string
-    }
-    return reject(new Error('The API call failed')); // Provide an error message
-  })
-    .finally(() => {
-      console.log('Got a response from the API'); // This will run after the promise settles
+  return promise
+    .then(() => {
+      console.log('Got a response from the API'); // Log on resolution
+      return { status: 200, body: 'success' }; // Return success object
+    })
+    .catch(() => {
+      console.log('Got a response from the API'); // Log on rejection as well
+      return new Error(); // Return empty Error object
     });
 }
