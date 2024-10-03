29aaa6bf89123e40ef03efe90c0943e3c326fd10
konyhea <ndukaprincek@gmail.com>
2024-10-03 03:51:29 +0100

    usage of resolve

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,11 +1,5 @@
 export default function signUpUser(firstName, lastName) {
-    return new Promise((resolve, reject) => {
-      const success = true;
-  
-      if (success) {
-        resolve({ firstName, lastName });
-      } else {
-        reject(new Error());
-      }
-    });
-  }
+  return new Promise((resolve) => {
+    resolve({ firstName, lastName });
+  });
+}
