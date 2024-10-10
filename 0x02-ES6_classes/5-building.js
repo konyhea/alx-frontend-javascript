2b04fd2736cdb885e77a3d360865e2012e36c576
konyhea <ndukaprincek@gmail.com>
2024-10-10 04:25:50 +0100

    revert

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    // Type checking to ensure sqft is a number
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-
-    // Prevent direct instantiation of the abstract class
     if (new.target === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+      throw new Error(`${this.constructor.name} is an abstract class and cannot be instantiated directly.`);
     }
 
-    // Store the attribute with an underscore
     this._sqft = sqft;
   }
 
@@ -19,8 +12,8 @@ export default class Building {
     return this._sqft;
   }
 
-  // Abstract method to be implemented by any subclass
+  // Abstract method that must be implemented in derived classes
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    throw new Error(`Class extending ${this.constructor.name} must override evacuationWarningMessage`);
   }
 }
